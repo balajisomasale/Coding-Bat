2201443973ab5cbc14b39b883c4ff929267f5094
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2023-02-24 00:03:54 -0400

    Create near_hundred.py

---
+++ Warmup_1/near_hundred.py
@@ -0,0 +1,14 @@
+Warmup-1 > near_hundred
+prev  |  next  |  chance
+Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+
+
+near_hundred(93) → True
+near_hundred(90) → True
+near_hundred(89) → False
+
+
+Sol : 
+
+def near_hundred(n):
+  return (abs(100-n)<=10) or (abs(200-n)<=10)
