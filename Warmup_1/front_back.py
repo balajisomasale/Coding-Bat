0adeec23cd92b6ff4c2d1d579cd07bd3c2e2c309
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2023-02-24 00:35:38 -0400

    Create front_back.py

---
+++ Warmup_1/front_back.py
@@ -0,0 +1,19 @@
+Warmup-1 > front_back
+prev  |  next  |  chance
+Given a string, return a new string where the first and last chars have been exchanged.
+
+
+front_back('code') → 'eodc'
+front_back('a') → 'a'
+front_back('ab') → 'ba'
+
+Sol : 
+  
+  def front_back(str):
+  if len(str) <= 1:
+    return str
+  
+  mid = str[1:len(str)-1]  # can be written as str[1:-1]
+  
+  # last + mid + first
+  return str[len(str)-1] + mid + str[0]
