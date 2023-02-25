94ba5e0ca0117a6973aec7646388eb9b43a3ecfd
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2023-02-25 18:40:51 -0400

    Create without_end.py

---
+++ String-1/without_end.py
@@ -0,0 +1,13 @@
+String-1 > without_end
+prev  |  next  |  chance
+Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+
+without_end('Hello') → 'ell'
+without_end('java') → 'av'
+without_end('coding') → 'odin'
+
+Sol : 
+  
+  def without_end(str):
+  return str[1:-1]
