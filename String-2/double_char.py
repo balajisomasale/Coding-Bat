c427fb236317e424e9d2649cf41242792ea463fb
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2023-02-25 18:48:30 -0400

    Create double_char.py

---
+++ String-2/double_char.py
@@ -0,0 +1,15 @@
+String-2 > double_char
+prev  |  next  |  chance
+Given a string, return a string where for every char in the original, there are two chars.
+
+
+double_char('The') → 'TThhee'
+double_char('AAbb') → 'AAAAbbbb'
+double_char('Hi-There') → 'HHii--TThheerree'
+
+
+def double_char(str):
+  result=""
+  for i in range(len(str)):
+    result=result+2*str[i]
+  return result
