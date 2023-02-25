2bbc7abc5331b97c28ac0892f72a2a564b3d55a6
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2023-02-25 18:03:52 -0400

    Create hello_name.py

---
+++ String-1/hello_name.py
@@ -0,0 +1,12 @@
+String-1 > hello_name
+prev  |  next  |  chance
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+
+hello_name('Bob') → 'Hello Bob!'
+hello_name('Alice') → 'Hello Alice!'
+hello_name('X') → 'Hello X!'
+
+
+def hello_name(name):
+  return "Hello "+name+"!"
