725c1765f472e62db589e1a1f4f768975e8c6169
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2023-02-25 15:51:17 -0400

    Create sorta_sum.py

---
+++ Logic-1/sorta_sum.py
@@ -0,0 +1,16 @@
+Logic-1 > sorta_sum
+prev  |  next  |  chance
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+
+
+sorta_sum(3, 4) → 7
+sorta_sum(9, 4) → 20
+sorta_sum(10, 11) → 21
+
+Sol : 
+
+def sorta_sum(a, b):
+  if a+b in range(10,20):
+    return 20
+  else:
+    return a+b
